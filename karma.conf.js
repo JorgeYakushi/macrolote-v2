42168d0394cca1ec4834e8e81cfcf4b49fe4acca
Jorge Yakushi <jym990@gmail.com>
2022-08-18 13:36:03 -0500

    project name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/macrolote-client"),
+      dir: require("path").join(__dirname, "./coverage/macrolote-v2"),
       reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true,
     },
